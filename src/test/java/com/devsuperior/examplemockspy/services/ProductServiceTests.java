37b283194a4834a659139336fe1dac92f91b2030
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-09 15:55:52 -0300

    Utilizando o Spy para mockar evitar erro

--- src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -2,7 +2,9 @@ package com.devsuperior.examplemockspy.services;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -37,7 +39,7 @@ public class ProductServiceTests {
 		existingId = 1L;
 		nonExistingId = 2L;
 		
-		product = new Product(1L, "PLaystation", 2000.00);
+		product = new Product(1L, "Playstation", 2000.00);
 		productDTO = new ProductDTO(product);
 		
 		Mockito.when(repository.save(any())).thenReturn(product);
@@ -46,4 +48,18 @@ public class ProductServiceTests {
 		Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
 		
 	}
+	
+	
+	@Test// Deve inserir e retornar produto dto quando valido...
+	public void insertShouldReturnProductDTOWhenValidDate() {
+		
+		ProductService serviceSpy = Mockito.spy(productService);
+		
+		Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+		ProductDTO result = serviceSpy.insert(productDTO);
+		
+		Assertions.assertNotNull(result);//testar se retorna vazio
+	    Assertions.assertEquals(result.getName(), "Playstation");// ver se o nome e igual a Playstation
+	}
+
 }
