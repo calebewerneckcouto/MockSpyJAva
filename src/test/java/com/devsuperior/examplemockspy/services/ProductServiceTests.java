a7104f412afb728dac3f46ad4f2903a0c32a5a5b
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-09 16:04:39 -0300

    insert com dados invalidos do poduto

--- src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.devsuperior.examplemockspy.dto.ProductDTO;
 import com.devsuperior.examplemockspy.entities.Product;
 import com.devsuperior.examplemockspy.repositories.ProductRepository;
+import com.devsuperior.examplemockspy.services.exceptions.InvalidDataException;
 
 import jakarta.persistence.EntityNotFoundException;
 @ExtendWith(SpringExtension.class)
@@ -61,5 +62,40 @@ public class ProductServiceTests {
 		Assertions.assertNotNull(result);//testar se retorna vazio
 	    Assertions.assertEquals(result.getName(), "Playstation");// ver se o nome e igual a Playstation
 	}
+	
+	
+	@Test//Deve retornar uma escecao caso o nome seja em branco ou nulo....
+	public void insertShouldRertunrInvalidDataExceptionWhenProductNameIsBlanck() {
+		
+		productDTO.setName("");
+		
+		ProductService serviceSpy = Mockito.spy(productService);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(InvalidDataException.class,()->{
+			@SuppressWarnings("unused")
+			ProductDTO result = serviceSpy.insert(productDTO);
+		});
+		
+		
+	}
+	
+	
+	@Test//Deve retornar uma escecao caso o preco negativo ou nulo
+	public void insertShouldRertunrInvalidDataExceptionWhenProductPriceIsNegativeOrZero() {
+		
+		productDTO.setPrice(-5.0);
+		
+		ProductService serviceSpy = Mockito.spy(productService);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(InvalidDataException.class,()->{
+			@SuppressWarnings("unused")
+			ProductDTO result = serviceSpy.insert(productDTO);
+		});
+		
+		
+	}
+	
 
 }
